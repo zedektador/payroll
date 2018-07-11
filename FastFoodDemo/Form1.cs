b443bbac33997c25e254f5b86196580af0eb7ed3
zedektador <zedekyaniii.za@gmail.com>
2018-07-11 09:18:45 +0800

    first commit

---
+++ FastFoodDemo/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FastFoodDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SidePanel.Height = button1.Height;
+            SidePanel.Top = button1.Top;
+            firstCustomControl1.BringToFront();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SidePanel.Height = button1.Height;
+            SidePanel.Top = button1.Top;
+            firstCustomControl1.BringToFront();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SidePanel.Height = button2.Height;
+            SidePanel.Top = button2.Top;
+            mySecondCustmControl1.BringToFront();
+
+            //Thanks for watching Friends...
+            //Please dont forget to Subscribe... :) :) :) 
+        }
+
+        private void panel1_MouseHover(object sender, EventArgs e)
+        {
+            panel1.Size = new Size(209, 583);
+            mySecondCustmControl1.Size = new Size(811, 406);
+        }
+
+        private void panel1_MouseLeave(object sender, EventArgs e)
+        {
+            panel1.Size = new Size(63, 583);
+            mySecondCustmControl1.Size = new Size(957, 406);
+        }
+    }
+}
